83c0a7056ad30d19e9e44229dcf4b34305c68fe3
VAN NGUYEN <nguyen.vance@gmail.com>
2018-07-20 23:50:26 -0700

    fixed bug in Items file

--- routes/api/Items.js
+++ routes/api/Items.js
@@ -9,8 +9,8 @@ const Item = require('../../models/Items');
 // @access  Public
 router.get('/', (req, res) => {
     Item.find()
-        .sort({date:-1})
-})
-
+        .sort({ date: -1 })
+        .then(items => res.json(items))
+});
 
 module.exports = router;
