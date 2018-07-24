06419391ad25ede56e82b1c4ce772ac138c77822
VAN NGUYEN <nguyen.vance@gmail.com>
2018-07-23 23:40:59 -0700

    issue with redux compose and store. init debugging

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
-import {connect} from 'react-redux';
-import {getItems} from '../actions/itemActions';
+import { connect } from 'react-redux';
+import { getItems } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
@@ -12,8 +12,8 @@ class ShoppingList extends Component {
         this.props.getItems();
     }
 
-    render(){
-        const { items } = this.state;
+    render() {
+        const { items } = this.props.item;
         return(
             <Container>
                 <Button 
