14e36c66501d1a9a442a6184a83f4fe634537a80
VAN NGUYEN <nguyen.vance@gmail.com>
2018-07-24 18:10:50 -0700

    completed. int deployment

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,12 +1,7 @@
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        // { id: uuid (), name: 'Eggs'},
-        // { id: uuid (), name: 'Milk'},
-        // { id: uuid (), name: 'Steak'},
-        // { id: uuid (), name: 'Water'}
-    ],
+    items: [],
     loading: false
 };
 
